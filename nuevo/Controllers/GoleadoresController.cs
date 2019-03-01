f2bc335add8dfd79891165a24391b9b84a9223ef
Clinton TL <44932378+Clinton1998@users.noreply.github.com>
2019-03-01 17:06:37 -0500

    Mi primer commit

---
+++ nuevo/Controllers/GoleadoresController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ejemplo_del.Models;
+
+
+namespace Ejemplo_del.Controllers
+{
+    public class GoleadoresController : Controller
+    {
+        JugadoresModelo JugadoresModelo;
+       
+        public GoleadoresController()
+        {
+            JugadoresModelo = new JugadoresModelo();
+        }
+        [ChildActionOnly]
+        public ActionResult _Goleadores()
+        {
+            List<JugadoresModelo.GOLEADORES> lista = this.JugadoresModelo.GetGoleadores();
+            return PartialView(lista);
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+
+
+}
